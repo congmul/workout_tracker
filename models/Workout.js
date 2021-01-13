1d13ac305bc0ffdebdd1b3d8a6b55c34b9740dca
Jehyun <congmul@hotmail.com>
2021-01-13 10:12:41 -0800

    Delete Exercises.js

--- models/Workout.js
+++ models/Workout.js
@@ -1,22 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const Schema = mongoose.Schema;
-
-// const WorkoutSchema = new Schema({
-//     day: {
-//       type: Date,
-//       default: Date.now
-//     }, 
-//     exercises: []
-//   });
-
-// const Workout = mongoose.model("Workout", WorkoutSchema);
-
-// module.exports = Workout;
-
-
-// New Schema with Exercises.js
-
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
